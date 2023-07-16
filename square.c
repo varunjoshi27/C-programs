1fcd48ea6c9e3935f4d37252b4707e556d47e9de
varunjoshi27 <121236149+varunjoshi27@users.noreply.github.com>
2023-07-16 22:32:48 +0530

    Add files via upload
    In this program I have made a program to give the square of any number given by the user.

---
+++ square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    float side;
+
+    printf("Enter the side\n");
+    scanf("%f",&side);
+
+    printf("The area of the square is %f\n",side*side);
+
+    return 0;
+}
